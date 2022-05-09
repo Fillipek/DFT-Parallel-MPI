fe4ae6efbbf9b0143d931559653362f8b701192b
Fillipek <marcin.filipek.321@gmail.com>
2022-05-09 21:26:11 +0200

    separate arrys for all threads

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <complex.h>
 #include "mpi.h"
 
-#define PROC_MASTER 1
+#define PROC_MASTER 0
 
 int main(int argc, char *argv[])
 {
@@ -16,62 +16,73 @@ int main(int argc, char *argv[])
 
     // inicjalizacja parametrów i zmiennych
     double complex *f_vals;        // array of function values
-
-    // wczytanie danych wejściowych
-    if (argc <= 1)
-    {
-        fprintf(stderr, "ERROR: The program requires an argument which is a path to data file.\n");
-        exit(EXIT_FAILURE);
-    }
-
     FILE *fp;
+    size_t N = 0;
     size_t len = 0;
     char *current_line = NULL;
     size_t line_len;
-    size_t n_lines = 0;
 
-    fp = fopen(argv[1], "r");
-    while ((line_len = getline(&current_line, &len, fp)) != -1)
+    if (myid == PROC_MASTER)
     {
-        n_lines++;
+        // wczytanie danych wejściowych
+        if (argc <= 1)
+        {
+            fprintf(stderr, "ERROR: The program requires an argument which is a path to data file.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        fp = fopen(argv[1], "r");
+        while ((line_len = getline(&current_line, &len, fp)) != -1)
+        {
+            N++;
+        }
+
+        fclose(fp);
+        if (current_line)
+            free(current_line);
     }
 
-    fclose(fp);
-    if (current_line)
-        free(current_line);
+    MPI_Bcast(&N, 1, MPI_UNSIGNED_LONG, PROC_MASTER, MPI_COMM_WORLD);
+    f_vals = malloc(sizeof(double complex) * N);
 
-    f_vals = malloc(sizeof(double complex) * n_lines);
-
-    fp = fopen(argv[1], "r");
-    complex double *f_curr = f_vals;
-    while ((line_len = getline(&current_line, &len, fp)) != -1)
+    if (myid == PROC_MASTER)
     {
-        *f_curr = (double)atof(current_line);
-        // printf("%.4lf + i%.4lf\n", f_curr->re, f_curr->im);
-        f_curr++;
+        fp = fopen(argv[1], "r");
+        complex double *f_curr = f_vals;
+        while ((line_len = getline(&current_line, &len, fp)) != -1)
+        {
+            *f_curr = (double)atof(current_line);
+            f_curr++;
+        }
+        fclose(fp);
+        if (current_line)
+            free(current_line);
     }
-    fclose(fp);
-    if (current_line)
-        free(current_line);
+
+    MPI_Bcast(f_vals, N, MPI_C_DOUBLE_COMPLEX, PROC_MASTER, MPI_COMM_WORLD);
+
 
     // wypisanie funckji
     // funkcja licząca FFT
 
-    dft_forward(f_vals, n_lines);
+    dft_forward(f_vals, N);
 
 
     // zachowanie wyników FFT
-    fp = fopen("data/fft_data.csv", "w");
-    for (int i =0; i<n_lines; i++)
+    if (myid == PROC_MASTER)
     {
-        fprintf(fp, "%d, %.4lf, %.4lf\n", i, creal(f_vals[i]), cimag(f_vals[i]));
+        FILE *fp = fopen("data/fft_data.csv", "w");
+        for (int i =0; i<N; i++)
+        {
+            fprintf(fp, "%d, %.4lf, %.4lf\n", i, creal(f_vals[i]), cimag(f_vals[i]));
+        }
+        fclose(fp);
     }
-    fclose(fp);
 
     // filtracja sygnału (odcięcie wartości których moduł < 50% max moduł)
     // liczymy kwadraty, czyli granica to 25% max
     // double module_max_sq = 0, module_curr_sq;
-    // for (double complex *p=f_vals; p!=f_vals+n_lines; p++)
+    // for (double complex *p=f_vals; p!=f_vals+N; p++)
     // {
     //     module_curr_sq = p->re * p->re + p->im * p->im;
     //     if (module_curr_sq > module_max_sq)
@@ -80,7 +91,7 @@ int main(int argc, char *argv[])
     //     }
     // }
     // double threshold = 0.25 * module_max_sq;
-    // for (double complex *p=f_vals; p!=f_vals+n_lines; p++)
+    // for (double complex *p=f_vals; p!=f_vals+N; p++)
     // {
     //     module_curr_sq = p->re * p->re + p->im * p->im;
     //     if (module_curr_sq < threshold)
@@ -90,15 +101,18 @@ int main(int argc, char *argv[])
     // }
 
     // policzneie FFT-1
-    dft_backward(f_vals, n_lines);
+    dft_backward(f_vals, N);
 
     // zachowanie wyników FFT-1
-    fp = fopen("data/rev_fft_data.csv", "w");
-    for (int i =0; i<n_lines; i++)
+    if (myid == PROC_MASTER)
     {
-        fprintf(fp, "%d, %.4lf, %.4lf\n", i, creal(f_vals[i]), cimag(f_vals[i]));
+        FILE *fp = fopen("data/rev_fft_data.csv", "w");
+        for (int i =0; i<N; i++)
+        {
+            fprintf(fp, "%d, %.4lf, %.4lf\n", i, creal(f_vals[i]), cimag(f_vals[i]));
+        }
+        fclose(fp);
     }
-    fclose(fp);
 
     // szprzątanie
     free(f_vals);
